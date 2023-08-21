e8b4999eaf395688693b46744fbecd8b2edc0273
Lenovo <Lenovo@DESKTOP-9SAQ5T8>
2023-08-21 17:35:01 +0530

    hi guys this is my first project....

---
+++ src/gitrepository/gitrepository.java
@@ -0,0 +1,17 @@
+package gitrepository;
+
+public class gitrepository {
+
+
+		public static void main(String[] args) {
+			StringBuilder st=new StringBuilder("Bobby");
+			String s=st.toString();
+			System.out.println(s);
+			// TODO Auto-generated method stub
+
+		}
+
+	}
+
+
+
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module gitrepository {
+}
